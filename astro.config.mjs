22ba57fd91a4dae8ca05d72ee16db0c11ab163bd
luiscode100 <luiscode100@gmail.com>
2025-10-16 11:44:35 +0200

    github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://luiscode100.github.io',
-  base: '/TU_REPO/'
+  base: '/portfolio_luis/'
 })
