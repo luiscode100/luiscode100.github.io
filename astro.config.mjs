08450d2bace18626b1cd5bd8dcf86280838be414
luiscode100 <luiscode100@gmail.com>
2025-10-16 13:50:37 +0200

    klkllk

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://luiscode100.github.io',
-  base: '/portfolio_luis/'
+  base: 'portfolio_luis'
 });
