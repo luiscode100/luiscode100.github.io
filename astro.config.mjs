5dbe1b93766f14f6296bc370797da236a7cf134d
luiscode100 <luiscode100@gmail.com>
2025-10-16 10:59:52 +0200

    init portfolio astro + tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://luiscode100.github.io',
+  base: '/TU_REPO/'
+})
